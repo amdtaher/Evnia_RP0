1beb79c7bf45e4728991cea740219cff0ce653f2
Tanveer <101932506+amdtaher@users.noreply.github.com>
2023-12-07 20:36:22 +0600

    Add files via upload

--- src/Components/Summary/Summary.jsx
+++ src/Components/Summary/Summary.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { Fragment } from 'react'
 import CountUp from 'react-countup';
 import VisibilitySensor from 'react-visibility-sensor';
@@ -74,4 +75,82 @@ const Summary = () => {
   )
 }
 
+=======
+import React, { Fragment } from 'react'
+import CountUp from 'react-countup';
+import VisibilitySensor from 'react-visibility-sensor';
+import { Container, Row, Col, Card, Button} from 'react-bootstrap';
+
+const Summary = () => {
+  return (
+    <Fragment>
+        <Container fluid={true} className='SummaryBanner p-0'>
+            <div className='SummaryBannerOverlay'>
+                <Container className='Summary'>
+                    <Row>
+                        <Col sm={12} md={6} lg={3} className='col'>
+                            <div className='SingleSummary'>
+                                <h4 className='SummaryHeading s1'>
+                                    <CountUp start={0} end={60} duration={5} suffix="+">
+                                    {({ countUpRef, start }) => (
+                                        <VisibilitySensor onChange={start} delayedcall>
+                                        <span ref={countUpRef} />
+                                        </VisibilitySensor>
+                                    )}
+                                    </CountUp>
+                                </h4>
+                                <p className='SummarySubHeading'>Project Finished</p>
+                            </div>
+                        </Col>
+                        <Col sm={12} md={6} lg={3} className='col'>
+                            <div className='SingleSummary'>
+                                <h4 className='SummaryHeading s2'>
+                                    <CountUp start={0} end={40} duration={5} suffix="+">
+                                    {({ countUpRef, start }) => (
+                                        <VisibilitySensor onChange={start} delayedcall>
+                                        <span ref={countUpRef} />
+                                        </VisibilitySensor>
+                                    )}
+                                    </CountUp>
+                                </h4>
+                                <p className='SummarySubHeading'>Feedbacks</p>
+                            </div>
+                        </Col>
+                        <Col sm={12} md={6} lg={3} className='col'>
+                            <div className='SingleSummary'>
+                                <h4 className='SummaryHeading s3'>
+                                    <CountUp start={0} end={50} duration={5} suffix="+">
+                                    {({ countUpRef, start }) => (
+                                        <VisibilitySensor onChange={start} delayedcall>
+                                        <span ref={countUpRef} />
+                                        </VisibilitySensor>
+                                    )}
+                                    </CountUp>
+                                </h4>
+                                <p className='SummarySubHeading'>Happy Clients</p>
+                            </div>
+                        </Col>
+                        <Col sm={12} md={6} lg={3} className='col'>
+                            <div className='SingleSummary'>
+                                <h4 className='SummaryHeading s4'>
+                                    <CountUp start={0} end={5} duration={6} suffix="+">
+                                    {({ countUpRef, start }) => (
+                                        <VisibilitySensor onChange={start} delayedcall>
+                                        <span ref={countUpRef} />
+                                        </VisibilitySensor>
+                                    )}
+                                    </CountUp>
+                                </h4>
+                                <p className='SummarySubHeading'>Stars Given</p>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        </Container>
+    </Fragment>
+  )
+}
+
+>>>>>>> 050df2dc8d7e254ce9dad4de43fb7382a3919130
 export default Summary
