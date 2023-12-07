9ab46c420007f072f7e06e1f8c05f72a04998719
Tanveer <101932506+amdtaher@users.noreply.github.com>
2023-12-07 20:49:14 +0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import TopFixedBannner from './Components/TopFixedBannner/TopFixedBannner'
 import About from './Components/About/About'
 import TopNavigation from './Components/TopNavigation/TopNavigation'
 import Features from './Components/Features/Features'
+import Summary from './Components/Summary/Summary'
+import Popup from './Components/Popup/Popup'
+import Analysis from './Components/Analysis/Analysis'
+import Todo from './Components/todo/Todo'
+import Testimonial from './Components/Testimonial/Testimonial'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +21,11 @@ function App() {
       <TopFixedBannner/> 
       <About/>
       <Features/>
+      <Summary/>
+      <Popup/>
+      <Analysis/>
+      <Todo/>
+      <Testimonial/>
     </>
   )
 }
